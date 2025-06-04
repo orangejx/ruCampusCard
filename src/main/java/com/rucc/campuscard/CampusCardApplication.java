5073e526666ffc6e301a620e348abe8eda5a5a31
Aldrich J. Xing <aldrichx@wlms.dev>
2025-06-04 09:14:20 +0800

    init.

---
+++ src/main/java/com/rucc/campuscard/CampusCardApplication.java
@@ -0,0 +1,13 @@
+package com.rucc.campuscard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusCardApplication.class, args);
+    }
+
+}
